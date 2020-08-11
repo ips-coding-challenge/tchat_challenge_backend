fdc91cec121e21cde6440feaff89f317076fbd17
myself <android.programmation@laposte.net>
2020-08-11 17:50:15 +0200

    Ready to deploy... I thing ;)

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,24 @@
-import path from 'path';
-import favicon from 'serve-favicon';
-import compress from 'compression';
-import helmet from 'helmet';
-import cors from 'cors';
+import path from "path";
+import favicon from "serve-favicon";
+import compress from "compression";
+import helmet from "helmet";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
-import feathers from '@feathersjs/feathers';
-import configuration from '@feathersjs/configuration';
-import express from '@feathersjs/express';
-import socketio from '@feathersjs/socketio';
+import feathers from "@feathersjs/feathers";
+import configuration from "@feathersjs/configuration";
+import express from "@feathersjs/express";
+import socketio from "@feathersjs/socketio";
 
-
-import { Application } from './declarations';
-import logger from './logger';
-import middleware from './middleware';
-import services from './services';
-import appHooks from './app.hooks';
-import channels from './channels';
-import authentication from './authentication';
-import mongoose from './mongoose';
+import { Application } from "./declarations";
+import logger from "./logger";
+import middleware from "./middleware";
+import services from "./services";
+import appHooks from "./app.hooks";
+import channels from "./channels";
+import authentication from "./authentication";
+import mongoose from "./mongoose";
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const app: Application = express(feathers());
@@ -26,13 +27,17 @@ const app: Application = express(feathers());
 app.configure(configuration());
 // Enable security, CORS, compression, favicon and body parsing
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+  })
+);
 app.use(compress());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
+app.use(favicon(path.join(app.get("public"), "favicon.ico")));
 // Host the public folder
-app.use('/', express.static(app.get('public')));
+app.use("/", express.static(app.get("public")));
 
 // Set up Plugins and providers
 app.configure(express.rest());
