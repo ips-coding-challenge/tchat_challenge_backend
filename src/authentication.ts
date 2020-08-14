769b2aec09b2578dae51d88d4e57ceebcdb8cfe8
myself <android.programmation@laposte.net>
2020-08-14 07:27:06 +0200

    Add some server logs

--- src/authentication.ts
+++ src/authentication.ts
@@ -1,15 +1,15 @@
-import { ServiceAddons, Params } from '@feathersjs/feathers';
-import { AuthenticationService, JWTStrategy } from '@feathersjs/authentication';
-import { LocalStrategy } from '@feathersjs/authentication-local';
+import { ServiceAddons, Params } from "@feathersjs/feathers";
+import { AuthenticationService, JWTStrategy } from "@feathersjs/authentication";
+import { LocalStrategy } from "@feathersjs/authentication-local";
 import {
   expressOauth,
   OAuthStrategy,
   OAuthProfile,
-} from '@feathersjs/authentication-oauth';
+} from "@feathersjs/authentication-oauth";
 
-import { Application } from './declarations';
+import { Application } from "./declarations";
 
-declare module './declarations' {
+declare module "./declarations" {
   interface ServiceTypes {
     authentication: AuthenticationService & ServiceAddons<any>;
   }
@@ -19,6 +19,10 @@ class GitHubStrategy extends OAuthStrategy {
   async getEntityData(profile: OAuthProfile, existing: any, params: Params) {
     const baseData = await super.getEntityData(profile, existing, params);
 
+    console.log(`-----------------`);
+    console.log(`BaseData`, baseData);
+    console.log(`Existing`, existing);
+    console.log(`-----------------`);
     // If the user already exists i don't rewrite informations
     // Informations are now dependant from the application
     // So i keep name / avatar / email as there could have been updated at some point
@@ -39,10 +43,10 @@ class GitHubStrategy extends OAuthStrategy {
 export default function (app: Application): void {
   const authentication = new AuthenticationService(app);
 
-  authentication.register('jwt', new JWTStrategy());
-  authentication.register('local', new LocalStrategy());
-  authentication.register('github', new GitHubStrategy());
+  authentication.register("jwt", new JWTStrategy());
+  authentication.register("local", new LocalStrategy());
+  authentication.register("github", new GitHubStrategy());
 
-  app.use('/authentication', authentication);
+  app.use("/authentication", authentication);
   app.configure(expressOauth());
 }
--- src/services/messages/messages.class.ts
+++ src/services/messages/messages.class.ts
@@ -69,7 +69,6 @@ export class Messages extends Service {
           },
         },
       },
-
       {
         $unwind: "$messages",
       },
